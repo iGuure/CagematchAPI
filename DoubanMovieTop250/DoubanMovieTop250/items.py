32f9cc3910552e9c587004612ba3ee3f746122ef
Guure <543449492@qq.com>
2015-11-30 01:32:08 +0800

    Leviticus

--- DoubanMovieTop250/DoubanMovieTop250/items.py
+++ DoubanMovieTop250/DoubanMovieTop250/items.py
@@ -11,4 +11,5 @@ import scrapy
 class Doubanmovietop250Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    rank = scrapy.Field()
+    title = scrapy.Field()
