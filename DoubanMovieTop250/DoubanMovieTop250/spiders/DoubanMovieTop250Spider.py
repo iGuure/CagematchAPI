5ff99c809168b9f7ba5d4a1cd3cdd13fb490b2c3
Guure <543449492@qq.com>
2015-11-30 01:28:21 +0800

    Exodus

---
+++ DoubanMovieTop250/DoubanMovieTop250/spiders/DoubanMovieTop250Spider.py
@@ -0,0 +1,21 @@
+import scrapy
+from DoubanMovieTop250.items import Doubanmovietop250Item
+
+class DoubanMovieTop250Spider(scrapy.Spider):
+	name = 'doubanmovietop250'
+	allowed_domains = ["douban.com"]
+	start_urls = [
+		"http://movie.douban.com/top250/"
+	]
+
+	def parse(self, response):
+		for info in response.xpath('//div[@class="item"]'):
+			item = Doubanmovietop250Item()
+			item['rank'] = info.xpath('div[@class="pic"]/em/text()').extract()
+			item['title'] = info.xpath('div[@class="pic"]/a/img/@alt').extract()
+			yield item
+
+		nextPage = response.xpath('//span[@class="next"]/a/@href')
+		if nextPage:
+			url = response.urljoin(nextPage[0].extract())
+			yield scrapy.Request(url, self.parse)
