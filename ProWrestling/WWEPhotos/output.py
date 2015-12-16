f8f6b33a43e11d36cde0575f55fd85479cd9f8a4
Guure <543449492@qq.com>
2015-12-16 18:10:44 +0800

    Samuel

--- ProWrestling/WWEPhotos/output.py
+++ ProWrestling/WWEPhotos/output.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# This process is aimed at starting a picture post of WWE photos on a Discuz! forum.
+# This process is aimed at starting a picture post of WWE photos on a Discuz! forum
 
 import sys
 import json
@@ -9,19 +9,23 @@ def readWWEJson():
 	reload(sys)
 	sys.setdefaultencoding('utf-8')
 
-	inFile = open("items.json", 'r', 0)
+	inFile = open("items10.json", 'r', 0)
 	text = inFile.read()
 	WWEDict = json.loads(text)
 	preTitle = None
 	preSummary = None
 	for WWE in WWEDict:
 		title = WWE["title"][0]
-		summary = WWE["summary"][0]
 
-		if preTitle != title and preSummary != summary:
-			print "\n[b]".decode("utf-8") + title + "[/b]\n\n".decode("utf-8") + summary
+		if preTitle != title:
+			print "\n[b]".decode("utf-8") + title + "[/b]".decode("utf-8")
 			preTitle = title
-			preSummary = summary
+
+		if WWE["summary"]:
+			summary = WWE["summary"][0]
+			if preSummary != summary:
+				print "\n".decode("utf-8") + summary
+				preSummary = summary
 
 		picURL = WWE["picURL"][0]
 		if WWE["caption"]:
@@ -29,3 +33,5 @@ def readWWEJson():
 			print "\n[img]".decode("utf-8") + picURL + "[/img]\n\n".decode("utf-8") + caption
 		else:
 			print "\n[img]".decode("utf-8") + picURL + "[/img]".decode("utf-8")
+
+readWWEJson()
